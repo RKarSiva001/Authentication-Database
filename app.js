6e0685875909d61b4ab38661895dea72966e5671
KARTHICK R <85785728+RKarSiva001@users.noreply.github.com>
2024-01-14 18:10:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ const initializeBDAndServer = async () => {
 };
 
 initializeBDAndServer();
-
+/*
 app.post("/register", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
   const hashedPassword = await bcrypt.hash(request.body.password, 10);
   const selectUserQuery = `
-    SELECT * FROM user WHERE username = '${username};`;
+    SELECT * FROM user WHERE username = '${username}';`;
   const dbUser = await db.get(selectUserQuery);
 
   if (dbUser === undefined) {
@@ -52,16 +52,57 @@ app.post("/register", async (request, response) => {
     response.send("User already exists");
   }
 });
+*/
+app.post("/register", async (request, response) => {
+  let { username, name, password, gender, location } = request.body; //Destructuring the data from the API call
+
+  let hashedPassword = await bcrypt.hash(password, 10); //Hashing the given password
+
+  let checkTheUsername = `
+            SELECT *
+            FROM user
+            WHERE username = '${username}';`;
+  let userData = await db.get(checkTheUsername); //Getting the user details from the database
+  if (userData === undefined) {
+    //checks the condition if user is already registered or not in the database
+    /*If userData is not present in the database then this condition executes*/
+    let postNewUserQuery = `
+            INSERT INTO
+            user (username,name,password,gender,location)
+            VALUES (
+                '${username}',
+                '${name}',
+                '${hashedPassword}',
+                '${gender}',
+                '${location}'
+            );`;
+    if (password.length < 5) {
+      //checking the length of the password
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      /*If password length is greater than 5 then this block will execute*/
+
+      let newUserDetails = await db.run(postNewUserQuery); //Updating data to the database
+      response.status(200);
+      response.send("User created successfully");
+    }
+  } else {
+    /*If the userData is already registered in the database then this block will execute*/
+    response.status(400);
+    response.send("User already exists");
+  }
+});
 
 app.post("/login", async (request, response) => {
   const { username, password } = request.body;
   const selectUserQuery = `
-    SELECT * FROM user WHERE username = '${username};`;
+    SELECT * FROM user WHERE username = '${username}';`;
   const dbUser = await db.get(selectUserQuery);
 
   if (dbUser === undefined) {
     response.status(400);
-    response.send("Invalid User");
+    response.send("Invalid user");
   } else {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
 
@@ -94,7 +135,7 @@ app.put("/change-password", async (request, response) => {
       } else {
         const encryptedPassword = await bcrypt.hash(newPassword, 10);
         const updatePasswordQuery = `
-                UPDATE user SET password = '${encryptedPassword}' WHERE username = '${username};`;
+                UPDATE user SET password = '${encryptedPassword}' WHERE username = '${username}';`;
         await db.run(updatePasswordQuery);
         response.send("Password updated");
       }
@@ -104,3 +145,5 @@ app.put("/change-password", async (request, response) => {
     }
   }
 });
+
+module.exports = app;
